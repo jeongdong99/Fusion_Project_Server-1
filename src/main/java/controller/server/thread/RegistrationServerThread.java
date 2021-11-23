e7e05de44f8c2e3f8128cc8dccc4c5339894c0ae
충엽 이 <lcy042000@kumoh.ac.kr>
2021-11-23 18:52:14 +0900

    commit(thread)

--- src/main/java/controller/server/thread/RegistrationServerThread.java
+++ src/main/java/controller/server/thread/RegistrationServerThread.java
@@ -5,30 +5,52 @@ import java.net.Socket;
 
 public class RegistrationServerThread extends Thread{
     private Socket socket;
+    InputStream is;
+    BufferedReader reader;
+    OutputStream os;
+    BufferedWriter writer;
 
     public RegistrationServerThread(Socket socket){
         this.socket = socket;
     }
 
     public void run(){
-        InputStream is;
-        BufferedReader reader;
-        OutputStream os;
-        BufferedWriter writer;
+        open();
+        System.out.println("접속 완료");
 
-        try {
+        while(true){
+
+        }
+    }
+
+    private void open(){
+        try{
             is = socket.getInputStream();
             reader = new BufferedReader(new InputStreamReader(is));
             os = socket.getOutputStream();
             writer = new BufferedWriter(new OutputStreamWriter(os));
-
-            System.out.println("접속 완료");
-
-//            while(true){
-//
-//            }
-        } catch (IOException e) {
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
+
+    //로그인
+    private void login(){
+
+    }
+
+    //로그아웃
+    private  void logout(){
+
+    }
+
+    //교수 생성
+
+    //학생 생성
+
+    //교과목 생성
+
+    //교과목 수정
+
+    //교과목 삭제제
 }
