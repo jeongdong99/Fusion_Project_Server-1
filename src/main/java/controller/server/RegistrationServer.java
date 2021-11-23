d435fad60f14c4de49b84058e3e8099a50c07996
tu1014 <tu1014@naver.com>
2021-11-23 18:26:23 +0900

    create server

---
+++ src/main/java/controller/server/RegistrationServer.java
@@ -0,0 +1,32 @@
+package controller.server;
+
+import java.io.*;
+import java.net.*;
+
+
+public class RegistrationServer {
+
+    public static void main(String[] args) {
+
+        ServerSocket ss = null;
+        Socket conn = null;
+
+        try {
+
+            ss = new ServerSocket(3000);
+            System.out.println("Server socket created.Waiting for connection...");
+
+            conn = ss.accept();
+
+            System.out.println("Connection received from " + conn.getInetAddress().getHostName() + " : " + conn.getPort());
+            // create Thread class
+
+
+        }
+
+        catch(IOException e) {System.err.println("IOException"); }
+
+
+
+    }
+}
