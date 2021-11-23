a206f4b0bc3418f69eb2e03459b814b8febd7fbe
충엽 이 <lcy042000@kumoh.ac.kr>
2021-11-23 18:24:44 +0900

    commit

--- src/main/java/controller/server/thread/RegistrationServerThread.java
+++ src/main/java/controller/server/thread/RegistrationServerThread.java
@@ -1,5 +1,34 @@
 package controller.server.thread;
 
-public class RegistrationServerThread extends Thread{
+import java.io.*;
+import java.net.Socket;
 
+public class RegistrationServerThread extends Thread{
+    private Socket socket;
+
+    RegistrationServerThread(Socket socket){
+        this.socket = socket;
+    }
+
+    public void run(){
+        InputStream is;
+        BufferedReader reader;
+        OutputStream os;
+        BufferedWriter writer;
+
+        try {
+            is = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is));
+            os = socket.getOutputStream();
+            writer = new BufferedWriter(new OutputStreamWriter(os));
+
+            System.out.println("접속 완료");
+
+//            while(true){
+//
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
